410c3eeda5d1abf527436bf295e9a626059902c9
Bhuvanesh <145738429+bhuvanesh8148316271@users.noreply.github.com>
2023-09-22 13:11:56 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import First_React_Project from "./first_react_project";
+
+function App(){
+  return <div><First_React_Project/></div>
+}
+export default App;
---
+++ src/first_react_project.tsx
@@ -0,0 +1,17 @@
+
+
+import myImage from './images/ajith.png';
+
+
+function First_React_Project(){
+    return (
+        <div style={{marginLeft: '30px'}}>
+          
+          <img src={myImage} alt="My Image" style={{ width: '300px', height: 'auto'}} />
+          <h2>Thunivu U/A</h2>
+          <h3>RUNTIME - 2.32</h3>
+          
+        </div>
+      );
+}
+export default First_React_Project;
